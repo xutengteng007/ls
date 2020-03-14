1d88b74dedd474420217491c15847cdeb6bc63e4
Tengteng Xu (徐腾腾) <xutengteng@inspur.com>
2020-03-14 09:52:17 +0800

    main

---
+++ springcloudrouter/src/test/java/com/ls/springcloudrouter/SpringcloudrouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.springcloudrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudrouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
